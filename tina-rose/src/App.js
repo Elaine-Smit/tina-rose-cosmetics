7ce1262d9710171d2f647629d67363f544f5462a
Elaine-Smit <elaine.smit8@gmail.com>
2022-10-10 15:17:43 +0200

    top
    added top information
    meta fonts colours etc

--- tina-rose/src/App.js
+++ tina-rose/src/App.js
@@ -4,9 +4,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div class="top">
-          
+        <div className="top">
+          <p id="contacts">
+            <a href="mailto:support@tinarose.co.za"><i class="fa-solid fa-envelope"></i> : support@tinarose.co.za </a>
+            <span id="phone"><i class="fa-solid fa-phone"></i> : 068 2444 998</span>
+          </p>
+          <p id="socials">
+            <a href="https://www.facebook.com/TinaRoseCosmetics" target="_blank" rel="noreferrer" id="fb-top">
+              <i class="fa-brands fa-facebook"></i>
+            </a>
+          </p>
         </div>
+        <nav>
+          <ul className="">
+            <li>Welcome</li>
+            <li>Shop</li>
+            <li>Account</li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
